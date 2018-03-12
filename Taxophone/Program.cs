273d98cdf445708380e473afe4fe0bbb31c4ac37
subben <subben17@gmail.com>
2018-03-12 17:45:13 +0400

    add array sort

--- Taxophone/Program.cs
+++ Taxophone/Program.cs
@@ -17,38 +17,44 @@ namespace Taxophone
 
             pytti = Console.ReadLine(); //var_name=int.Parse(Console.ReadLine()); -или так?
 
-            // Console.WriteLine("Введите путь до папки с иконками");
 
             // string[] Ikon = new string[] { };
             
 
             DirectoryInfo taskDirectory = new DirectoryInfo(pytti);
             FileInfo[] Ikon = taskDirectory.GetFiles("*.png");
-            
+
 
             // string[] Ikon = DirectoryInfo.GetFiles(pytti, "*.png"); // список всех png файлов в директории указанной в переменной pytti
 
                 for (int i = 0; i < Ikon.Length; i++)
                 {
-                    Console.WriteLine(Ikon[i]);                                             
-                
+                    Console.WriteLine(Ikon[i]);
                 }
+                Console.ReadKey();
 
-            int temp;
-            for (int i = 0; i < Ikon.Length - 1; i++)
-            {
-                for (int j = i + 1; j < Ikon.Length; j++)
+            Array.Sort(Ikon);
+                for (int i = 0; i < Ikon.Length; i++)
                 {
-                    if (Ikon[i] > Ikon[j])
-                    {
-                        temp = Ikon[i];
-                        Ikon[i] = Ikon[j]; // просто печаль
-                        Ikon[j] = temp;
-                    }
+                    Console.WriteLine(Ikon[i]);
                 }
-            }
+                Console.ReadKey();
 
-            //Console.ReadKey(); вывод масива
+            /* int temp;
+             for (int i = 0; i < Ikon.Length - 1; i++)
+             {
+                 for (int j = i + 1; j < Ikon.Length; j++)
+                 {
+                     if (Ikon[i] > Ikon[j])
+                     {
+                         temp = Ikon[i];
+                         Ikon[i] = Ikon[j];
+                         Ikon[j] = temp;
+                     }
+                 }
+             }*/
+
+            //Console.ReadKey(); //вывод масива
         }
     }
 }
